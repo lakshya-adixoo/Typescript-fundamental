029c3de3a4dbe6f871c81e98d794d9dd7a8cd7b8
lakshya-adixoo <lakshya.shrivastava@adixoo.com>
2025-02-25 12:19:51 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+// // types
+
+// // numbers 
+// let arr : number = 1;
+// let arr1 : string ;
+// arr1 = "lKSHY";
+
+// let arr2 : [number , string , boolean] = [1 , "lakshya" , true ];
+
+// enum information {
+
+//     name = "lakshya" ,
+//     age = "22",
+//     residence = "indore"
+// }
+
+// console.log(information.residence)
+
+
+//type inference
+let a;
+
