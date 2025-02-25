7c1d872c6536608fb2fd62344cf1175e54d21bb4
lakshya-adixoo <lakshya.shrivastava@adixoo.com>
2025-02-25 12:56:15 +0530

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 // // types
-// // numbers 
-// var arr = 1;
-// var arr1;
-// arr1 = "lKSHY";
-// var arr2 = [1, "lakshya", true];
-// var information;
-// (function (information) {
-//     information["name"] = "lakshya";
-//     information["age"] = "22";
-//     information["residence"] = "indore";
-// })(information || (information = {}));
-// console.log(information.residence);
-
+var a = {
+    Section: "A",
+    name: "lakshya"
+};
--- index.ts
+++ index.ts
@@ -17,6 +17,41 @@
 // console.log(information.residence)
 
 
-//type inference
-let a;
+// //type inference
+// let a = 12 ;
+// let b = "string";
+// let c = true;
+
+// union and intersection type 
+
+// let variable : string | null ;
+
+// variable = "hello";
+
+//union 
+// function abcd(variable: string | number){
+//     if(typeof variable === 'string'){
+//         variable.length;
+//     }
+// }
+
+// abcd(23);
+// abcd("lakshya");
+
+//intersection
+
+// type classmate = {
+//     Section : String
+// }
+// type students = {
+//      name : String 
+// }
+
+// type classmateInNames = classmate & students ;
+
+// let a : classmateInNames = {
+// Section : "A",
+// name : "lakshya"
+// }
+
 
