7951f796be3296bc1cbf06d4e149f580a33e5b73
lakshya-adixoo <lakshya.shrivastava@adixoo.com>
2025-02-25 14:59:37 +0530

    third commit

--- index.ts
+++ index.ts
@@ -1,57 +1,95 @@
-// // types
+// // // types
 
-// // numbers 
-// let arr : number = 1;
-// let arr1 : string ;
-// arr1 = "lKSHY";
+// // // numbers 
+// // let arr : number = 1;
+// // let arr1 : string ;
+// // arr1 = "lKSHY";
 
-// let arr2 : [number , string , boolean] = [1 , "lakshya" , true ];
+// // let arr2 : [number , string , boolean] = [1 , "lakshya" , true ];
 
-// enum information {
+// // enum information {
 
-//     name = "lakshya" ,
-//     age = "22",
-//     residence = "indore"
-// }
+// //     name = "lakshya" ,
+// //     age = "22",
+// //     residence = "indore"
+// // }
 
-// console.log(information.residence)
+// // console.log(information.residence)
 
 
-// //type inference
-// let a = 12 ;
-// let b = "string";
-// let c = true;
+// // //type inference
+// // let a = 12 ;
+// // let b = "string";
+// // let c = true;
 
-// union and intersection type 
+// // union and intersection type 
 
-// let variable : string | null ;
+// // let variable : string | null ;
 
-// variable = "hello";
+// // variable = "hello";
 
-//union 
-// function abcd(variable: string | number){
-//     if(typeof variable === 'string'){
-//         variable.length;
+// //union 
+// // function abcd(variable: string | number){
+// //     if(typeof variable === 'string'){
+// //         variable.length;
+// //     }
+// // }
+
+// // abcd(23);
+// // abcd("lakshya");
+
+// //intersection
+
+// // type classmate = {
+// //     Section : String
+// // }
+// // type students = {
+// //      name : String 
+// // }
+
+// // type classmateInNames = classmate & students ;
+
+// // // let a : classmateInNames = {
+// // // Section : "A",
+// // // name : "lakshya"
+// // // }
+
+// // //primivite type alais
+
+// // // type sankhya = number ;
+// // // let b : sankhya;
+
+// // interface user {
+// //       name : string,
+// //       email : string,
+// //       mobile : number,
+// //       dept : string
+// // }
+
+// // function abcd (User : user){
+// //     User.mobile. 
+// // }
+
+// type Data = string | null ;
+
+// class Pendrive {
+//     company = "hp";
+//     price = 1200;
+//     data : null | string = null;
+
+
+//     putData (data : Data){
+//         console.log(`putting some data ${data}`);
+//         this.data = data;
+//     }
+
+//     getData(data:Data){
+//         console.log(data);
 //     }
 // }
 
-// abcd(23);
-// abcd("lakshya");
 
-//intersection
-
-// type classmate = {
-//     Section : String
-// }
-// type students = {
-//      name : String 
-// }
-
-// type classmateInNames = classmate & students ;
-
-// let a : classmateInNames = {
-// Section : "A",
-// name : "lakshya"
-// }
+// let new1 = new Pendrive();
+// new1.putData("12");
 
 
